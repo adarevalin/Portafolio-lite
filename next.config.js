00cbc7f67e51a36f8fac507cd161d26328a7b29f
Andres David <kyfunky1386@gmail.com>
2024-05-17 22:04:51 -0500

    mejoras video

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+/**
+ * @type {import{'next'}.nextConfig}
+ */
+
 const { i18n } = require('./next-i18next.config.js')
 
 const nextConfig = {
   i18n,
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized:true,
+  },
   reactStrictMode: true
 }
 
